b3aa1d0ff9a4330cb80bac46459884b649036d1a
João Victor de Aquino Netto Paciello <joaovictorpaciello@gmail.com>
2025-06-15 18:07:07 -0300

    fix: Corrigido link do WhatsApp na seção de redes sociais

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -5,7 +5,7 @@ const Socials = () => {
     <div className="flex flex-col gap-4 bg-black/40 p-4 rounded-lg shadow-lg fixed bottom-20 right-3">
       <a href="https://www.linkedin.com/in/joaovictorpaciello" target="_blank"><FaLinkedin className="text-[2rem] text-gray-400 hover:text-blue-500 transition-colors duration-300" /></a>
       <a href="https://github.com/JvPaciello" target="_blank"><FaGithub className="text-[2rem] text-gray-400 hover:text-purple-500 transition-colors duration-300"/></a>
-      <a href=""><FaWhatsappSquare className="text-[2rem] text-gray-400 hover:text-green-500 transition-colors duration-300"/></a>
+      <a href="https://wa.me/5522999651023"><FaWhatsappSquare className="text-[2rem] text-gray-400 hover:text-green-500 transition-colors duration-300"/></a>
     </div>
   );
 };
